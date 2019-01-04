92b2a0890318834b47ff4dbd87ea648c40d49bcf
mazhiyu <444177096@qq.com>
2019-01-04 11:06:11 +0800

    init project

---
+++ src/main/java/com/marsyoung/Test.java
@@ -0,0 +1,4 @@
+package com.marsyoung;
+
+public class Test {
+}
---
+++ src/main/java/com/marsyoung/config/OnsProperty.java
@@ -0,0 +1,11 @@
+package com.marsyoung.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "ons")
+public class OnsProperty {
+
+
+
+
+}
